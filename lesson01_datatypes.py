c6b3692f250cbf01413e122a90e0c7e2c69bc5af
DOGATTACKerwfsdvckncbipdvcjvhb <kavin.pratap@gmail.com>
2025-09-24 16:18:01 +0000

    Created datatypes lesson thingy

---
+++ lesson01_datatypes.py
@@ -0,0 +1,145 @@
+#hello
+
+# 1. STRINGS:
+
+print("hello")
+
+print("looooooooooooooooooooog")
+
+print("Hi \ntoday \nis six seven day")
+print("CHICKEN \tJOCKEY")
+
+# INTEGERS
+
+print(42)
+print(5-5*11)
+
+#Float
+
+print(0.4 + 0.4 -0.3)
+print(6+2.0)
+print(6+0.7+0.06+0.007+0.0006+0.00007)
+
+#BOOLEAN
+
+print("\n",True)
+
+x = 10
+if x+5 > 10:
+    print("YAY")
+else:
+    print("NO")
+
+
+print(1==2)
+print(1==1)
+print(1!=2)
+
+#HOW DO I KNOW WHAT DATA TYPE I AM WORKING WITH?????????????????
+print("\nDATA TYPE LOOKUP:\n")
+
+print(type("To see this message, wrap me in a print statement"))
+print(type(3))
+print(type(3.14))
+print(type(1 == 1))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
