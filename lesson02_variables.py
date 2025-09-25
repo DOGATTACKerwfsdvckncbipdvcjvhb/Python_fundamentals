401ec69130d180aea4acc0d56c50ca6befe05c39
DOGATTACKerwfsdvckncbipdvcjvhb <kavin.pratap@gmail.com>
2025-09-25 17:25:52 +0000

    Did variables and a challenge today

---
+++ lesson02_variables.py
@@ -0,0 +1,64 @@
+#CONCEPT VARIABLES
+#Variables store values and they are stored in a physical storage unit
+
+SIXSEVEN = "Hello world "
+
+password = SIXSEVEN+"sixsevendayatsixtyseven"
+
+EMAIL = "TH3SIXSEVENNATOR_AT_OMAIL.COM"
+
+print("\nPASSWORD: ",password)
+print("EMAIL: ",EMAIL,"\n\n")
+
+temperature = 72.5
+print(type(temperature))
+
+enabled = False
+is_complete = True
+print("\n\n", is_complete)
+is_complete = False
+print("\n", is_complete)
+
+x = 3.14
+y = 7
+
+print("\n\n", x+y, "\n\n")
+
+count2 = 10
+while count2 >= 0:
+    
+    print(count2)
+    count2 = count2-1
+   
+#Challenge 1: rename variables
+x = "Radia Perlman"
+y = 34
+z = "Networking Engineer"
+
+name = "Radia Perlman"
+age = 34
+job = "Networking Engineer"
+
+#Challenge 2: Update Variables
+count = 10
+count = count + 5
+print("\n\n", count)
+
+#challenge 3: Swap variables
+
+x = 4
+y = "hello"
+temporary = 0
+
+temporary = y
+y = x
+x = temporary
+
+print("\n\nThis is X: ", x)
+print("This is Y: ", y)
+
+#Challenge 4: Boolean practice
+is_raining = False
+print("\n\nIs Raining: ", is_raining)
+is_raining = True
+print("Is Raining: ", is_raining)
