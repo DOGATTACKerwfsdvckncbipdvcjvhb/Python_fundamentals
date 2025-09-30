2ab871ab25d39129f398f13acd70c5f333112098
DOGATTACKerwfsdvckncbipdvcjvhb <kavin.pratap@gmail.com>
2025-09-30 15:53:55 +0000

    Adds math library examples

---
+++ lesson04_libraries.py
@@ -0,0 +1,23 @@
+#Python libraries, libraries are things that add functions to make coding easier, it is very useful.
+import math
+
+print("Square root: ", math.sqrt(25))
+print("Round up to integer: ", math.ceil(4.2)) #round up is ceil as in ceiling because the ceiling is up.
+print("Round down to integer: ", math.floor(4.8)) #round down is floor as in flooring because the flooring is down.
+print("Power: ", math.pow(2,5)) #pow is power for exponents like 2^5 because pow is the first 3 letters of power
+print("Pi: ", math.pi) #pi is for pi because pi is 3.141592...
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
