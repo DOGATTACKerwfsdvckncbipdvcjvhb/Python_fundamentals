65b5104b34c9aebd8d40c17ddc1fda29d85f1577
DOGATTACKerwfsdvckncbipdvcjvhb <kavin.pratap@gmail.com>
2025-10-27 17:02:28 +0000

    While loop

--- lesson09_loops.py
+++ lesson09_loops.py
@@ -14,17 +14,17 @@ import time
 c = 0
 for thing in thoongs:
     print("Looking at the thing: ",thing)
-    time.sleep(1.5)
-    x = input("what do you think will be looked at next: ")
-    x.lower()
-    c = c+1
-    if x == thoongs[c]:
-        print("Yay you got it, now we leave")
-        break
-    else:
-        print("you got it wrong")
-        print("going to next thing")
-        print("thing number you on: ",c)
+    time.sleep(0.1)
+    # x = input("what do you think will be looked at next: ")
+    # x.lower()
+    # c = c+1
+    # if x == thoongs[c]:
+    #     print("Yay you got it, now we leave")
+    #     break
+    # else:
+    #     print("you got it wrong")
+    #     print("going to next thing")
+    #     print("thing number you on: ",c)
         
     
 
@@ -41,6 +41,52 @@ for i in range(2,11,2):
 #break can allow it to go out of function early I learned this in C/C++
 #continue goes to the start of a loop
 
+print("\n\n\n---Iterating over strings---")
 
 
 
+favWORD = "LINGANGUUUU"
+
+letter_list = []
+
+for letter in favWORD:
+    print(letter,end=" ")
+    letter_list.append(letter)
+    print(letter_list)
+
+print()
+print(letter_list,"\n")
+
+#WHILE LOOPS
+#while works with conditionals and for does not in python, so sad fors are so much better in C
+#SIXSEVEN
+
+print("---While loops---")
+
+count = 0
+while count < 5:
+    print(f"looping, we are on loop number {count}")
+    count += 1
+    time.sleep(0.1)
+
+print("done with loop")
+userinput = ''
+while userinput != 'exit':
+    userinput = input("type 'exit' to escape: ").lower()
+    if userinput == 'exit':
+        print("Exited")
+
+countbutcool = 60
+increment = 1
+
+while countbutcool > 0:
+    
+    if countbutcool-increment< 0:
+        print("Emergency exit")
+        break
+    countbutcool-= increment
+    increment+=1
+
+    print("countdown: ", countbutcool)
+    
+    time.sleep(0.05)
